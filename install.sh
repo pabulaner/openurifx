ceb228f7412ba5650dec156cbe8a072dd18c4b72
Paul H <p.hayder@icloud.com>
2025-02-04 11:41:58 +0100

    Updated install.sh and created local maven repo

--- install.sh
+++ install.sh
@@ -1,34 +1,33 @@
 #!/bin/zsh
 
+# fixed version
+fixedVersion=21-internal+0-2025-02-04-102810
+
 #
 # install faulty version
 #
 
 # build dmg
-mvn clean install jpackage:jpackage -Djfx-graphics.version=24-ea+19
-
-# install dmg
-pkill OpenUriFxApp
-hdiutil attach ./target/dist/OpenUriFxApp-1.0.dmg
-sudo cp -R /Volumes/OpenUriFxApp/OpenUriFxApp.app /Applications
-hdiutil unmount /Volumes/OpenUriFxApp
-
-# open app
-open /Applications/OpenUriFxApp.app
-
-echo Waiting 10 seconds for execution of fixed version...
-sleep 10
+#mvn clean install jpackage:jpackage -Djfx-graphics.version=21
+#
+## install dmg
+#pkill OpenUriFxApp
+#hdiutil attach ./target/dist/OpenUriFxApp-1.0.dmg
+#sudo cp -R /Volumes/OpenUriFxApp/OpenUriFxApp.app /Applications
+#hdiutil unmount /Volumes/OpenUriFxApp
+#
+## open app
+#open /Applications/OpenUriFxApp.app
+#
+#echo Waiting 10 seconds for execution of fixed version...
+#sleep 10
 
 #
 # install fixed version
 #
 
-# copy fixed javafx-graphics jar to ~/.m2/repository
-mkdir -p ~/.m2/repository/org/openjfx/javafx-graphics/24-internal+0-2025-01-20-190918/
-cp -R ./javafx-graphics/* ~/.m2/repository/org/openjfx/javafx-graphics/24-internal+0-2025-01-20-190918/
-
 # build dmg
-mvn clean install jpackage:jpackage
+mvn clean install jpackage:jpackage -Djfx-graphics.version=$fixedVersion
 
 # install dmg
 pkill OpenUriFxApp
