b4769a7ad572ec9b864d76836539be61b96a5801
Paul H <p.hayder@icloud.com>
2025-02-04 11:42:53 +0100

    Uncommented some lines inside install.sh

--- install.sh
+++ install.sh
@@ -8,19 +8,19 @@ fixedVersion=21-internal+0-2025-02-04-102810
 #
 
 # build dmg
-#mvn clean install jpackage:jpackage -Djfx-graphics.version=21
-#
-## install dmg
-#pkill OpenUriFxApp
-#hdiutil attach ./target/dist/OpenUriFxApp-1.0.dmg
-#sudo cp -R /Volumes/OpenUriFxApp/OpenUriFxApp.app /Applications
-#hdiutil unmount /Volumes/OpenUriFxApp
-#
-## open app
-#open /Applications/OpenUriFxApp.app
-#
-#echo Waiting 10 seconds for execution of fixed version...
-#sleep 10
+mvn clean install jpackage:jpackage -Djfx-graphics.version=21
+
+# install dmg
+pkill OpenUriFxApp
+hdiutil attach ./target/dist/OpenUriFxApp-1.0.dmg
+sudo cp -R /Volumes/OpenUriFxApp/OpenUriFxApp.app /Applications
+hdiutil unmount /Volumes/OpenUriFxApp
+
+# open app
+open /Applications/OpenUriFxApp.app
+
+echo Waiting 10 seconds for execution of fixed version...
+sleep 10
 
 #
 # install fixed version
